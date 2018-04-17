2463a74b135175b0f390df276a3e45ab14df3f15
Jessica Robertson <jalarimar@gmail.com>
2018-04-18 10:14:35 +1200

    list of questions on index page

--- prototype/questions/models.py
+++ prototype/questions/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    question_text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.question_text
--- prototype/questions/views.py
+++ prototype/questions/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Question
 
 # Create your views here.
+class IndexView(generic.ListView):
+    template_name = 'questions/index.html'
+    context_object_name = 'question_list'
+
+    def get_queryset(self):
+        return Question.objects.order_by('question_text')
