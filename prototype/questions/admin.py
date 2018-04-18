8d4427416a80bfc818c31f0feda797689f9b0c4c
Jessica Robertson <jalarimar@gmail.com>
2018-04-19 11:11:25 +1200

    Add skill area and test case models

--- prototype/questions/admin.py
+++ prototype/questions/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import *
 
-# Register your models here.
+
 admin.site.register(Question)
+admin.site.register(TestCase)
+admin.site.register(SkillArea)
--- prototype/questions/models.py
+++ prototype/questions/models.py
@@ -1,9 +1,25 @@
 from django.db import models
 
-# Create your models here.
+SMALL = 100
+LARGE = 500
+
 class Question(models.Model):
-    title = models.CharField(max_length=100)
-    question_text = models.CharField(max_length=500)
+    title = models.CharField(max_length=SMALL)
+    question_text = models.CharField(max_length=LARGE)
+    test_cases = models.ManyToManyField('TestCase')
+    skill_areas = models.ManyToManyField('SkillArea')
 
     def __str__(self):
         return self.question_text
+
+
+class SkillArea(models.Model):
+    name = models.CharField(max_length=SMALL)
+
+    def __str__(self):
+        return self.name
+
+
+class TestCase(models.Model):
+    test_input = models.CharField(max_length=SMALL)
+    expected_output = models.CharField(max_length=LARGE)
