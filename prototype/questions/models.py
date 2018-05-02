fe330ef29749620713c569fc0ae6584624a61e63
Jessica Robertson <jalarimar@gmail.com>
2018-05-02 15:58:01 +1200

    Add code to test user programs against test cases, well done message if all test cases  passed, poll sphere engine from client instead of server.

--- prototype/questions/models.py
+++ prototype/questions/models.py
@@ -13,6 +13,8 @@ class Token(models.Model):
 class Question(models.Model):
     title = models.CharField(max_length=SMALL)
     question_text = models.CharField(max_length=LARGE)
+    question_type = models.SmallIntegerField()
+    function_name = models.CharField(max_length=SMALL, blank=True)
     test_cases = models.ManyToManyField('TestCase')
     skill_areas = models.ManyToManyField('SkillArea', related_name='questions')
 
@@ -28,8 +30,17 @@ class SkillArea(models.Model):
 
 
 class TestCase(models.Model):
-    test_input = models.CharField(max_length=SMALL)
+    function_params = models.CharField(max_length=LARGE, blank=True)
+    test_input = models.CharField(max_length=LARGE, blank=True)
     expected_output = models.CharField(max_length=LARGE)
 
     def __str__(self):
-        return self.test_input + ' -> ' + self.expected_output
+        if len(self.function_params) > 0:
+            i = ''
+            if len(self.test_input) > 0:
+                i = self.test_input + ' + '
+            return i + 'f(' + self.function_params + ') -> ' + self.expected_output
+        elif len(self.test_input) > 0:
+            return self.test_input + ' -> ' + self.expected_output
+        else:
+            return self.expected_output
--- prototype/questions/views.py
+++ prototype/questions/views.py
@@ -31,47 +31,98 @@ class QuestionView(generic.DetailView):
 BASE_URL = "http://36adab90.compilers.sphere-engine.com/api/v3/submissions/"
 TOKEN = "?access_token=" + Token.objects.get(pk='sphere').token
 PYTHON = 116
-SUCCESS = 15
+COMPLETED = 0
+PROGRAM = 1
+FUNCTION = 2
+
+PROGRAM_ABOVE = """
+import json
+n = 0
+m = 0
+correct = [False] * len(test_outputs)
+def input(prompt=""):
+    global n
+    real_print(prompt)
+    test_input = test_inputs[n]
+    if n >= len(test_inputs):
+        raise EOFError()
+    n += 1
+    return test_input
+real_print = print
+def print(user_output):
+    global m
+    if m < len(test_outputs):
+        expected_output = test_outputs[m]
+        if user_output == expected_output:
+            correct[m] = True
+        m += 1
+"""
+
+PROGRAM_BELOW = """
+if n < len(test_inputs):
+    raise Exception('Input was not called enough times')
+real_print(json.dumps(correct))
+"""
+
+def add_program_test_code(question, user_code):
+    test_cases = question.test_cases.all()
+    
+    test_inputs = "\ntest_inputs = ["
+    test_outputs = "\ntest_outputs = ["
+    repeated_user_code = """\n"""
+
+    for case in test_cases:
+        input_str = repr(case.test_input) + ", "
+        output_str = repr(case.expected_output) + ", "
+        code_str = user_code + "\n"
+
+        test_inputs += input_str
+        test_outputs += output_str
+        repeated_user_code += code_str
+        
+    test_inputs = test_inputs[:-2] + "]\n"
+    test_outputs = test_outputs[:-2] + "]\n"
+
+    complete_code = test_inputs + test_outputs + PROGRAM_ABOVE + repeated_user_code + PROGRAM_BELOW
+
+    return complete_code
+
+
+def add_function_test_code(question, user_code):
+    test_cases = question.test_cases.all()
+
 
 def send_code(request):
     code = request.POST.get('user_input')
+    question_id = request.POST.get('question')
+    question = Question.objects.get(pk=question_id)
+
+    if question.question_type == PROGRAM:
+        code = add_program_test_code(question, code)
+    elif question.question_type == FUNCTION:
+        code = add_function_test_code(question, code)
     
     response = requests.post(BASE_URL + TOKEN, data = {"language": PYTHON, "sourceCode": code})
-    result = {
-       "data": response.json()
-    }
+    result = response.json()
 
     return JsonResponse(result)
 
 def get_output(request):
-    execution_id = request.GET.get('id')
+    submission_id = request.GET.get('id')
     question_id = request.GET.get('question')
 
-    data = {
-        "status": -1
+    params = {
+        "withOutput": True, 
+        "withStderr": True, 
+        "withCmpinfo": True
     }
-    while data["status"] != 0:
-        time.sleep(3)
-        params = {
-            "withOutput": True, 
-            "withStderr": True, 
-            "withCmpinfo": True
-        }
-        response = requests.get(BASE_URL + execution_id + TOKEN, params=params)
-        data = response.json()
+    response = requests.get(BASE_URL + submission_id + TOKEN, params=params)
+    result = response.json()
 
-    if data["result"] == SUCCESS:
-        output = data["output"]
-        question = Question.objects.get(pk=question_id)
-        test_cases = question.test_cases.all()
-        expected_output = test_cases.first().expected_output
-
-        if output[:-1] == expected_output:
-            data["correct"] = True
-        else:
-            data["correct"] = False
+    if result["status"] == COMPLETED:
+        result["completed"] = True
     else:
-        data["correct"] = False
+        result["completed"] = False
 
-    return JsonResponse(data)
+    return JsonResponse(result)
     
