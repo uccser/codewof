7826b2f2c789370a1909cbb310073ab394045b1e
Jessica Robertson <jalarimar@gmail.com>
2018-05-27 17:15:57 +1200

    Selenium test

--- prototype/questions/tests/test_integrations.py
+++ prototype/questions/tests/test_integrations.py
@@ -40,3 +40,29 @@ class SignUpTestCase(StaticLiveServerTestCase):
         submit.send_keys(Keys.RETURN)
         time.sleep(1)
         assert 'Prototype 402' in selenium.title
+    
+    def test_register_bad_password(self):
+        selenium = self.selenium
+        selenium.get(self.live_server_url)
+        sign_up_button = selenium.find_element_by_link_text('Sign Up')
+        sign_up_button.click()
+
+        username = selenium.find_element_by_id('id_username')
+        first_name = selenium.find_element_by_id('id_first_name')
+        last_name = selenium.find_element_by_id('id_last_name')
+        email = selenium.find_element_by_id('id_email')
+        password1 = selenium.find_element_by_id('id_password1')
+        password2 = selenium.find_element_by_id('id_password2')
+        
+        submit = selenium.find_element_by_id('sign_up_button')
+
+        username.send_keys('wizard')
+        last_name.send_keys('Potter')
+        email.send_keys('ur-a-wizard@uclive.ac.nz')
+        password1.send_keys('harrypotter')
+        password2.send_keys('harrypotter')
+
+        time.sleep(1)
+        submit.send_keys(Keys.RETURN)
+        time.sleep(1)
+        assert 'too similar to the last name' in selenium.page_source
