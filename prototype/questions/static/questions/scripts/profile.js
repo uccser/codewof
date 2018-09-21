49bc8e5eda6648de0c6a420cbe82ffa33f3abee1
Jessica Robertson <jalarimar@gmail.com>
2018-09-21 21:56:08 +1200

    Link fitness gauge to goal metric

--- prototype/questions/static/questions/scripts/profile.js
+++ prototype/questions/static/questions/scripts/profile.js
@@ -1,5 +1,21 @@
+var fitness_score = 100;
+var recency_weights = [0.5, 2, 1.5, 0.75, 0.25];
+
+for (var i = 0; i < 5; i++) {
+    var n_attempts = $("#week" + i).text();
+    var n_target = $('#goal_choice').val();
+
+    if (+n_attempts < +n_target) {
+        
+        var percent_away_from_goal = 1 - (n_attempts / n_target);
+        var penalty = 20 * recency_weights[i] * percent_away_from_goal;
+
+        fitness_score -= penalty;
+    }
+}
+
 $('.demo').kumaGauge({
-    value : 100,
+    value : fitness_score,
     radius: 90,
     gaugeWidth: 40,
     fill : '0-#fa4133:0-#ffaa00:35-#fdee37:50-#1cb42f:100',
