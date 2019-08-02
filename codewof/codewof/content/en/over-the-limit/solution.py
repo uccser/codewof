e444db2f4a1b5d49f33acb14d30889c89e4ef98c
ltt19 <ltt19@uclive.ac.nz>
2019-08-02 12:58:01 +1200

    Adding questions - Bus Info, Greeting, Over the Limit, Print codeWOF, String Concatenation, and Voting Age

---
+++ codewof/codewof/content/en/over-the-limit/solution.py
@@ -0,0 +1,5 @@
+def over_alcohol_limit(mcg_per_litre):
+    if mcg_per_litre > 250:
+        return True
+    else:
+        return False
---
+++ codewof/codewof/content/en/voting-age/solution.py
@@ -0,0 +1,5 @@
+age = int(input("How old are you? "))
+if age >= 18:
+    print("You are old enough to vote")
+else:
+    print("You are not old enough to vote yet")
