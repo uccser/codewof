ec214c171be76e02ed340f18a6b59afce2662984
JackMorganNZ <jackmorgannz@gmail.com>
2019-08-06 13:09:19 +1200

    Handle users with existing study registration

--- codewof/research/views.py
+++ codewof/research/views.py
@@ -2,6 +2,8 @@
 
 from django.views import generic
 from django.contrib import messages
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
 from django.template import Context
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
@@ -46,19 +48,41 @@ class StudyDetailView(generic.DetailView):
         studies = Study.objects.filter(visible=True).order_by('start_date')
         return studies
 
+    def get_context_data(self, **kwargs):
+        """Get additional context data for template."""
+        context = super().get_context_data(**kwargs)
+        try:
+            registration = StudyRegistration.objects.get(
+                user=self.request.user,
+                study_group__in=self.object.groups.all(),
+            )
+        except ObjectDoesNotExist:
+            registration = None
+        context['registration'] = registration
+        return context
 
 class StudyConsentFormView(LoginRequiredMixin, FormView):
 
-    form_class = MaintainingProgrammingSkills2019Form
-
     template_name = 'research/study_consent_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.study = Study.objects.get(
+            pk=self.kwargs.get('pk'),
+        )
+        try:
+            StudyRegistration.objects.get(
+                user=self.request.user,
+                study_group__in=self.study.groups.all(),
+            )
+        except ObjectDoesNotExist:
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            return redirect(self.study)
 
     def get_context_data(self, **kwargs):
         """Get additional context data for template."""
         context = super().get_context_data(**kwargs)
-        context['study'] = Study.objects.get(
-            pk=self.kwargs.get('pk'),
-        )
+        context['study'] = self.study
         return context
 
     def get_initial(self):
@@ -68,8 +92,7 @@ class StudyConsentFormView(LoginRequiredMixin, FormView):
         return initial
 
     def get_form_class(self):
-        study = Study.objects.get(pk=self.kwargs.get('pk'))
-        return get_consent_form_class(study.consent_form)
+        return get_consent_form_class(self.study.consent_form)
 
     def form_valid(self, form):
         study = Study.objects.get(
@@ -91,6 +114,6 @@ class StudyConsentFormView(LoginRequiredMixin, FormView):
         # )
         messages.success(
             self.request,
-            'You are successfully enrolled into the "{}" study. You have been emailed a copy of the signed consent for'.format(study.title)
+            'You are successfully enrolled into this study. You have been emailed a copy of your signed consent form.'.format(study.title)
         )
         return redirect(study)
