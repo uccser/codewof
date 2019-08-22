6e37b13f06eb47f800305d87300a396a45b28ea6
Gerard MacManus <gmacmanus@student.stbedes.school.nz>
2019-08-22 16:11:14 +1200

    rugbyscore question

--- codewof/programming/content/en/rugbyscore/solution.py
+++ codewof/programming/content/en/rugbyscore/solution.py
@@ -1,10 +1,2 @@
-def rideshare(km,airportfee):
-    fee=0
-    tripfee=0
-    if airportfee == "yes":
-        fee = fee + 6.5
-    totalfee=km*2.7
-    total=totalfee+fee
-    total=total*1.15
-    total = round(total,2)
-    print(total)
+def rugbyscore(tries,conversions,penalties):
+    print(tries*5+conversions*2+penalties*3)
