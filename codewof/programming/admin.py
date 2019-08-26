80f882f56ac818feade75b5f31eec96bb2a7f06f
JackMorganNZ <jackmorgannz@gmail.com>
2019-08-27 06:19:46 +1200

    Show whitespace in admin read-only mode of attempts
    Fixes #58

--- codewof/programming/admin.py
+++ codewof/programming/admin.py
@@ -2,9 +2,16 @@
 
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from programming.models import Attempt, QuestionTypeDebugging
+from programming.models import Attempt, TestCaseAttempt
 
 User = get_user_model()
+
+
+class TestCaseAttemptInline(admin.TabularInline):
+    model = TestCaseAttempt
+    fields = ()
+    extra = 0
+    can_delete = False
 
 
 class AttemptAdmin(admin.ModelAdmin):
@@ -12,7 +19,14 @@ class AttemptAdmin(admin.ModelAdmin):
 
     list_display = ('datetime', 'question', 'profile', 'passed_tests')
     ordering = ('-datetime', )
+    inlines = (TestCaseAttemptInline, )
+
+    class Media:
+        """Add extra CSS rules."""
+
+        css = {
+            "all": ("css/admin.css",)
+        }
 
 
 admin.site.register(Attempt, AttemptAdmin)
-admin.site.register(QuestionTypeDebugging)
