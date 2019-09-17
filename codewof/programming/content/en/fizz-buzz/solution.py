24e264c72aa446d778bf0d9b55eb32be8eb7b37d
Caitlin Duncan <caitlinduncan1@gmail.com>
2019-09-17 13:48:49 +1200

    Modified the fizzbuzz solution and extra parsons lines to simplify the parsons problem

--- codewof/programming/content/en/fizz-buzz/solution.py
+++ codewof/programming/content/en/fizz-buzz/solution.py
@@ -1,11 +1,9 @@
 def fizz_buzz(num):
-    divisible_by_3 = num % 3 == 0
-    divisible_by_5 = num % 5 == 0
-    if divisible_by_3 and divisible_by_5:
-        print('FizzBuzz')
-    elif divisible_by_3:
-        print('Fizz')
-    elif divisible_by_5:
-        print('Buzz')
+    if (num % 3 == 0) and (num % 5 == 0):
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
     else:
         print(num)
