b13c77d181d019d8077b0b221c0fe0c7efe44841
eAlasdair <eAlasdair@gmail.com>
2019-09-19 10:13:47 +1200

    simplify passtheparcel

--- codewof/programming/content/en/pass-the-parcel/initial.py
+++ codewof/programming/content/en/pass-the-parcel/initial.py
@@ -1,8 +1,6 @@
 def pass_the_parcel(parcel):
-    number_of_gifts = len(parcel)
-    person_to_receive_gift = 1
-    for i in range(number_of_gifts, 0, -1):
+    number_of_gifts = len(percel)
+    for i in range(0, number_of_gifts, 1):
         gift = parcel[i]
-        print("Person " + str(person_receive_gift) + " got " + gift + "!")
-        person_to_recieve_gift += 1
+        print("Person " + str(i) + " got " + gift + "!")
     print("The parcel is empty!")
--- codewof/programming/content/en/pass-the-parcel/solution.py
+++ codewof/programming/content/en/pass-the-parcel/solution.py
@@ -1,8 +1,6 @@
 def pass_the_parcel(parcel):
     number_of_gifts = len(parcel)
-    person_to_receive_gift = 1
-    for i in range(number_of_gifts, 0, -1):
-        gift = parcel[i - 1]
-        print("Person " + str(person_to_receive_gift) + " got " + gift + "!")
-        person_to_receive_gift += 1
+    for i in range(0, number_of_gifts, 1):
+        gift = parcel[i]
+        print("Person " + str(i + 1) + " got " + gift + "!")
     print("The parcel is empty!")
