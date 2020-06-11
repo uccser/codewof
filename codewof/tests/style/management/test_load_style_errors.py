5ac9eb06a0efe7b0fb2ee036be05d6a552ff7095
JackMorganNZ <jackmorgannz@gmail.com>
2020-06-11 23:14:54 +1200

    Add test cases

---
+++ codewof/tests/style/management/test_load_style_errors.py
@@ -0,0 +1,14 @@
+"""Module for the testing custom Django load_style_errors commands."""
+
+from django.test import TestCase
+from django.core import management
+
+
+class LoadStyleErrorsCommandTest(TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.language = "en"
+
+    def test_load_style_errors_valid(self, topic_loader, age_loader):
+        management.call_command("load_style_errors")
