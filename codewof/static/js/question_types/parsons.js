7d62199cacbdd3c6a9978481f88b3d7ecfaaf597
courtneycb <courtneybracefield86@gmail.com>
2020-08-19 13:39:05 +1200

    Add parsons tut.

--- codewof/static/js/question_types/parsons.js
+++ codewof/static/js/question_types/parsons.js
@@ -1,5 +1,6 @@
 var base = require('./base.js');
 var Sortable = require('sortablejs');
+const introJS = require('intro.js');
 
 var test_cases = {};
 var indent_increment = '    ';
@@ -29,6 +30,11 @@ $(document).ready(function(){
         data = test_cases_list[i];
         test_cases[data.id] = data
     }
+
+    setTutorialAttributes();
+    $("#tutorial").click(function() {
+        introJS.introJs().start();
+    });
 });
 
 function run_code(submit) {
@@ -121,4 +127,52 @@ function run_python_code(user_code, test_case) {
         test_case.received_output = 'No Python code provided.';
         test_case.runtime_error = true;
     }
+}
+
+
+function setTutorialAttributes() {
+    $(".question-text").attr(
+        'data-intro',
+        'This is the question you will be solving. It will tell you what your code should return (or print) for certain inputs.'
+    );
+    $("#available-lines").attr(
+        'data-intro',
+        "These are the lines you can choose from to build your solution. Drag and drop the lines you want into the 'Your solution' box. Not all lines may be needed for the solution."
+    );
+    $("#user-code-lines").attr(
+        'data-intro',
+        "This is where you will drop lines from the 'Available lines' section to build your solution. This is the code that will be run against the test cases when you click the 'Run code' button."
+    );
+    $("#test-case-table").attr(
+        'data-intro',
+        "These are the test cases that will be run against your code."
+    );
+    // the first row in the test case table
+    $('#test-case-table tbody tr:nth-child(1)').attr(
+        'data-intro',
+        'Here is the first test case.'
+    );
+    // the input for the first test case
+    $('#test-case-table tbody tr:nth-child(1) td:eq(0)').attr(
+        'data-intro',
+        'This is how your code will be called for this particular test.'
+    );
+    // the expected output for the first test case
+    $('#test-case-table tbody tr:nth-child(1) td:eq(1)').attr(
+        'data-intro',
+        'This is the output that the test expects your code to return (or print) for the given input.'
+    );
+    // the received output for the first test case
+    $('#test-case-table tbody tr:nth-child(1) td:eq(2)').attr(
+        'data-intro',
+        'This is the output that your code has returned (or printed) for the given input.'
+    );
+    // the status of the first test case
+    $('#test-case-table tbody tr:nth-child(1) td:eq(3)').attr(
+        'data-intro',
+        "This is the status of the test. It will say one of three things: 'Not yet run', 'Passed' or 'Failed'.\
+        'Not yet run' means your code has not yet been run against the test case.\
+        'Passed' means the received output matched the expected output. The test case has passed.\
+        'Failed' means the received output did not match the expected output. The test case has failed."
+    );
 }
