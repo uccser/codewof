99d75002529fa10c86f1e8e2f0690378c130264b
Jackie Qiu <jqi26@uclive.ac.nz>
2021-07-19 18:39:24 +1200

    Implemented the Invitation Accept and Reject button functionality.
    - invitations.js: Finished implementing the two event functions.
    - invitation-card.html: Prepended 'invitation-' to the card id.
    - dashboard.html: Added JS to get the url for an invitation to accept and reject.

--- codewof/static/js/groups/invitations.js
+++ codewof/static/js/groups/invitations.js
@@ -1,22 +1,59 @@
+/**
+ * Iterates through all the Invitation Accept and Reject buttons and adds onclick listeners.
+ */
 $(document).ready(function () {
      let invitationAcceptLinks = document.getElementsByClassName("invitation-accept-link");
      for (let link of invitationAcceptLinks) {
-        let invitationID = link.parentNode.parentNode.id
+         let invitationID = getID(link.parentNode.parentNode.id)
          link.onclick = function () { acceptInvitation(invitationID) }
      }
 
      let invitationRejectLinks = document.getElementsByClassName("invitation-reject-link");
      for (let link of invitationRejectLinks) {
-        let invitationID = link.parentNode.parentNode.id
+        let invitationID = getID(link.parentNode.parentNode.id)
          link.onclick = function () { rejectInvitation(invitationID) }
      }
 })
 
 
-function acceptInvitation(invitationID) {
-    console.log("Accepted: " + invitationID)
+/**
+ * Extracts the number portion of the id.
+ * @param full The original HTML element id, intended to be the form of <text>-<number>.
+ * @returns {number} The number part of the id as an int.
+ */
+function getID(full) {
+    return parseInt(full.substr(full.indexOf("-") + 1, full.length));
 }
 
+
+/**
+ * Sends an ajax request to accept an Invitation, then refreshes the page upon success.
+ * @param invitationID The ID of the Invitation to accept.
+ */
+function acceptInvitation(invitationID) {
+    $.ajax({
+        type: "POST",
+        url: getAcceptURL(invitationID),
+        async: true,
+        cache: true,
+        headers: {"X-CSRFToken": csrftoken},
+        success: function(data, textStatus, xhr) { location.reload(); },
+    });
+}
+
+/**
+ * Sends an ajax request to reject an Invitation, and removes the card for that invitation.
+ * @param invitationID The ID of the Invitation to reject.
+ */
 function rejectInvitation(invitationID) {
-    console.log("Rejected: " + invitationID)
+    $.ajax({
+        type: "DELETE",
+        url: getRejectURL(invitationID),
+        async: true,
+        cache: true,
+        headers: {"X-CSRFToken": csrftoken},
+        success: function(data, textStatus, xhr) {
+            document.getElementById("invitation-" + invitationID).remove()
+        },
+    });
 }
