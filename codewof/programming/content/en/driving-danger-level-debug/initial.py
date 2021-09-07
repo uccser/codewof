92b6a2cfac1fce97673bb62ebf4a14cc67a69fdf
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2021-09-08 10:29:19 +1200

    Added driving danger level question (function and debug)

--- codewof/programming/content/en/driving-danger-level-debug/initial.py
+++ codewof/programming/content/en/driving-danger-level-debug/initial.py
@@ -4,4 +4,4 @@ def check_danger_level(is_dark, is_wet):
     elif is_dark is True or is_wet is True:
         print('The driving conditions are currently dangerous!')
     else:
-        print("It is a nice day for a drive.")
+        print('It's is a nice day for a drive.')
