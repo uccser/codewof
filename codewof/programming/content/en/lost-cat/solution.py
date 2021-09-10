c3f9e3f02c66bd6ad2f4e8ec61dba0b70902582b
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2021-09-10 16:57:17 +1200

    Added lost cat question (program)

---
+++ codewof/programming/content/en/lost-cat/solution.py
@@ -0,0 +1,5 @@
+word = input("Enter search area: ")
+while word != 'food bowl':
+    print("Hmmm they are not in the " + word + "...")
+    word = input("Enter search area: ")
+print("There you are!")
