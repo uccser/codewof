2fa73c71f7ea3baf4e5fcf4d9244bce653785a85
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2021-09-10 16:16:19 +1200

    Added pen picker question (debugging)

---
+++ codewof/programming/content/en/pen-picker/initial.py
@@ -0,0 +1,4 @@
+def pick_pens(writing_colours, is_marking):
+    if is_marking is True:
+        writing_colours.add(red)
+    return writing_colours
---
+++ codewof/programming/content/en/pen-picker/solution.py
@@ -0,0 +1,4 @@
+def pick_pens(writing_colours, is_marking):
+    if is_marking is True:
+        writing_colours.append('red')
+    return writing_colours
