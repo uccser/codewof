5f5344e5c27f8432da900c3d3c4fd0e95b85eef0
Jackie Qiu <jqi26@uclive.ac.nz>
2021-09-19 17:02:03 +1200

    Removing unused import.

--- codewof/programming/serializers.py
+++ codewof/programming/serializers.py
@@ -2,7 +2,6 @@
 
 from rest_framework import serializers
 from programming.models import Question, Attempt, Profile, Like
-from users.serializers import UserSerializer
 
 
 class QuestionSerializer(serializers.ModelSerializer):
