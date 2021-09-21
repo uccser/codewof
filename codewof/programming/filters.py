419b902b003c9bd23a663dbf33e06c7430a20bd3
Rebekah <bexmckinnon@github.com>
2021-09-21 13:23:50 +1200

    Add filtering on contexts and concepts

--- codewof/programming/filters.py
+++ codewof/programming/filters.py
@@ -2,8 +2,7 @@ import django_filters
 from programming.models import Question
 
 class QuestionFilter(django_filters.FilterSet):
-    difficulty_level = django_filters.CharFilter(field_name="difficulty_level__name", lookup_expr='iexact')
-
+    
     class Meta:
         model = Question
-        fields = {'difficulty_level'}
+        fields = {'difficulty_level', 'concepts', 'contexts'}
