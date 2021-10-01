43fdd1072e925ece33ecb490a5477f1c99fa22b8
Jack Morgan <jackmorgannz@gmail.com>
2021-10-01 17:23:43 +1300

    Modify filterset to prefetch related attribute

--- codewof/programming/filters.py
+++ codewof/programming/filters.py
@@ -1,10 +1,23 @@
 import django_filters
-from programming.models import Question, DifficultyLevel
+from programming.models import (
+    Question,
+    DifficultyLevel,
+    ProgrammingConcepts,
+    QuestionContexts,
+)
 from django.db.models import Q
+
 
 class QuestionFilter(django_filters.FilterSet):
     # contexts=django_filters.filters.CharFilter(method="contexts_filter")
-    
+
+    concepts = django_filters.filters.ModelMultipleChoiceFilter(
+        queryset=ProgrammingConcepts.objects.prefetch_related('parent')
+    )
+    contexts = django_filters.filters.ModelMultipleChoiceFilter(
+        queryset=QuestionContexts.objects.prefetch_related('parent')
+    )
+
     class Meta:
         model = Question
         fields = {'difficulty_level', 'concepts', 'contexts'}
