035c2bf2e4cbd0b9dad5b4f623a81eb88e3278ae
Griffin Baxter <griffinrbaxter@gmail.com>
2022-08-08 15:47:51 +1200

    Complete question recommendations, uncomfortable difficulty recommending
    Question recommendations are now fully functional. Question recommendations have also been implemented for recommending questions at comfortable concepts and contexts, and "uncomfortable" difficulties, that are slightly higher than the user is used to.

--- codewof/programming/question_recommendations.py
+++ codewof/programming/question_recommendations.py
@@ -3,7 +3,9 @@
 import random
 import statistics
 
-from programming.models import DifficultyLevel, ProgrammingConcepts, QuestionContexts, Attempt, Question
+from django.db.models import Q
+
+from programming.models import DifficultyLevel, ProgrammingConcepts, QuestionContexts, Question
 from programming.skill_and_level_tracking import get_level_and_skill_info
 
 
@@ -16,89 +18,9 @@ def get_recommended_questions(profile):
     """Get the recommended questions based on the user's previously answered questions."""
     level_and_skill_info = get_level_and_skill_info(profile)
     scores = get_scores(level_and_skill_info)
-    recommended_questions = calculate_recommended_questions(profile, scores)
+    unsolved_questions = get_unsolved_questions(profile)
+    recommended_questions = calculate_recommended_questions(scores, unsolved_questions)
     return recommended_questions
-
-
-def calculate_recommended_questions(profile, scores):
-    """
-    Get the recommended questions from calculations based on the provided scores and user.
-
-    Retrieves the recommendation values for the difficulty, concept, and context, and uses this to calculate the
-    recommended questions.
-    """
-    comfortable_difficulties = get_comfortable_difficulties(scores['difficulty'])
-    uncomfortable_concepts = get_uncomfortable_concepts_or_contexts(scores['concept'], concept_numbers)
-    uncomfortable_contexts = get_uncomfortable_concepts_or_contexts(scores['context'], context_numbers)
-    comfortable_difficulty_recommendations = get_comfortable_difficulty_recommendations(
-        profile, comfortable_difficulties, uncomfortable_concepts, uncomfortable_contexts
-    )
-    recommended_questions = []
-    if len(comfortable_difficulty_recommendations) > 0:
-        recommended_questions.append(random.choice(comfortable_difficulty_recommendations))
-    return recommended_questions
-
-
-def get_comfortable_difficulty_recommendations(
-    profile, comfortable_difficulties, uncomfortable_concepts, uncomfortable_contexts
-):
-    """
-    Get the comfortable difficulty, uncomfortable concepts/contexts, question recommendations.
-
-    Iterates through possible combinations to find a valid question that matches the criteria.
-    """
-    solved_question_slugs = get_solved_question_slugs(profile)
-
-    questions = Question.objects.distinct('pk').select_subclasses()
-    initial_concepts = uncomfortable_concepts[0]
-    for difficulty in comfortable_difficulties:
-        comfortable_difficulty_questions = questions.filter(difficulty_level__level=difficulty)
-        for contexts in uncomfortable_contexts:
-            comfortable_difficulty_recommendations = calculate_comfortable_difficulty_questions(
-                solved_question_slugs, comfortable_difficulty_questions, initial_concepts, contexts
-            )
-            if len(comfortable_difficulty_recommendations) > 0:
-                return comfortable_difficulty_recommendations
-        for concepts in uncomfortable_concepts:
-            comfortable_difficulty_recommendations = calculate_comfortable_difficulty_questions(
-                solved_question_slugs, comfortable_difficulty_questions, concepts
-            )
-            if len(comfortable_difficulty_recommendations) > 0:
-                return comfortable_difficulty_recommendations
-        comfortable_difficulty_recommendations = calculate_comfortable_difficulty_questions(
-            solved_question_slugs, comfortable_difficulty_questions
-        )
-        if len(comfortable_difficulty_recommendations) > 0:
-            return comfortable_difficulty_recommendations
-    return []
-
-
-def get_solved_question_slugs(profile):
-    """Get the question slugs of those solved by the user."""
-    solved_question_slugs = set()
-    solved_attempts = Attempt.objects.filter(profile=profile, passed_tests=True).all()
-    for solved_attempt in solved_attempts:
-        solved_question_slugs.add(solved_attempt.question.slug)
-    return solved_question_slugs
-
-
-def calculate_comfortable_difficulty_questions(passed_question_slugs, questions, concepts=None, contexts=None):
-    """Calculate and return the comfortable difficulty, uncomfortable concepts/contexts, question recommendations."""
-    comfortable_difficulty_questions = []
-    for question in questions:
-        if question.slug not in passed_question_slugs:
-            filters = []
-            if concepts is not None:
-                question_concepts = [concept.number for concept in question.concepts.all()]
-                filters.append(
-                    any(uncomfortable_concept in question_concepts for uncomfortable_concept in concepts))
-            if contexts is not None:
-                question_contexts = [context.number for context in question.contexts.all()]
-                filters.append(
-                    any(uncomfortable_context in question_contexts for uncomfortable_context in contexts))
-            if all(filters):
-                comfortable_difficulty_questions.append(question)
-    return comfortable_difficulty_questions
 
 
 def get_scores(level_and_skill_info):
@@ -135,6 +57,107 @@ def generate_scores(numbers, info_category):
     return scores
 
 
+def get_unsolved_questions(profile):
+    """Get all questions unsolved by the user."""
+    return Question.objects.all().filter(
+        Q(attempt__isnull=True) | (Q(attempt__passed_tests=False, attempt__profile=profile))
+    ).distinct('pk').select_subclasses()
+
+
+def calculate_recommended_questions(scores, unsolved_questions):
+    """
+    Get the recommended questions from calculations based on the provided scores and user.
+
+    Retrieves the recommendation values for the difficulty, concept, and context, and uses this to calculate the
+    recommended questions.
+    """
+    comfortable_difficulty_questions, comfortable_concepts_contexts_questions = get_recommendation_categories(
+        scores, unsolved_questions
+    )
+    recommended_questions = get_random_recommendations(
+        comfortable_difficulty_questions, comfortable_concepts_contexts_questions
+    )
+    return recommended_questions
+
+
+def get_recommendation_categories(scores, unsolved_questions):
+    """Get the recommendations for all categories (comfortable difficulty, and comfortable concepts/contexts)."""
+    comfortable_difficulties = get_comfortable_difficulties(scores['difficulty'])
+    uncomfortable_concepts = get_uncomfortable_concepts_or_contexts(scores['concept'], concept_numbers)
+    uncomfortable_contexts = get_uncomfortable_concepts_or_contexts(scores['context'], context_numbers)
+    comfortable_difficulty_recommendations = get_recommendations(
+        unsolved_questions, comfortable_difficulties, uncomfortable_concepts, uncomfortable_contexts
+    )
+    uncomfortable_difficulties = get_uncomfortable_difficulties(comfortable_difficulties)
+    comfortable_concepts = list(reversed(uncomfortable_concepts))
+    comfortable_contexts = list(reversed(uncomfortable_contexts))
+    comfortable_concepts_contexts_recommendations = get_recommendations(
+        unsolved_questions, uncomfortable_difficulties, comfortable_concepts, comfortable_contexts
+    )
+    return comfortable_difficulty_recommendations, comfortable_concepts_contexts_recommendations
+
+
+def get_recommendations(questions, ordered_difficulties, ordered_concepts, ordered_contexts):
+    """
+    Get the question recommendations for the given ordered difficulty, concepts, and contexts.
+
+    Iterates through possible combinations to find valid questions that matches the criteria.
+    """
+    initial_concepts = ordered_concepts[0]
+    for difficulty in ordered_difficulties:
+        comfortable_difficulty_questions = questions.filter(difficulty_level__level=difficulty)
+        for contexts in ordered_contexts:
+            recommendations = calculate_comfortable_difficulty_questions(
+                comfortable_difficulty_questions, initial_concepts, contexts
+            )
+            if len(recommendations) > 0:
+                return recommendations
+        for concepts in ordered_concepts:
+            recommendations = calculate_comfortable_difficulty_questions(
+                comfortable_difficulty_questions, concepts
+            )
+            if len(recommendations) > 0:
+                return recommendations
+        recommendations = calculate_comfortable_difficulty_questions(
+            comfortable_difficulty_questions
+        )
+        if len(recommendations) > 0:
+            return recommendations
+    return []
+
+
+def calculate_comfortable_difficulty_questions(questions, concepts=None, contexts=None):
+    """Calculate and return the comfortable difficulty, uncomfortable concepts/contexts, question recommendations."""
+    comfortable_difficulty_questions = []
+    for question in questions:
+        filters = []
+        if concepts is not None:
+            question_concepts = [concept.number for concept in question.concepts.all()]
+            filters.append(
+                any(concept in question_concepts for concept in concepts))
+        if contexts is not None:
+            question_contexts = [context.number for context in question.contexts.all()]
+            filters.append(
+                any(context in question_contexts for context in contexts))
+        if all(filters):
+            comfortable_difficulty_questions.append(question)
+    return comfortable_difficulty_questions
+
+
+def get_random_recommendations(comfortable_difficulty_questions, comfortable_concepts_contexts_questions):
+    """Get a random (and unique) recommendation with questions from each recommendation category."""
+    random_recommendations = []
+    if len(comfortable_difficulty_questions) > 0:
+        comfortable_difficulty_choice = random.choice(comfortable_difficulty_questions)
+        random_recommendations.append(comfortable_difficulty_choice)
+        if comfortable_difficulty_choice in comfortable_concepts_contexts_questions:
+            comfortable_concepts_contexts_questions.remove(comfortable_difficulty_choice)
+    if len(comfortable_concepts_contexts_questions) > 0:
+        comfortable_concepts_contexts_choice = random.choice(comfortable_concepts_contexts_questions)
+        random_recommendations.append(comfortable_concepts_contexts_choice)
+    return random_recommendations
+
+
 def get_comfortable_difficulties(scores):
     """Return comfortable difficulty levels (reasonable for the user to solve, not too hard or easy, in-order)."""
     comfortable_difficulty = calculate_comfortable_difficulties(scores['month'])
@@ -146,7 +169,7 @@ def get_comfortable_difficulties(scores):
 
 
 def calculate_comfortable_difficulties(difficulty_scores):
-    """Calculate and return a comfortable difficulty levels (in-order) based on the supplied scores."""
+    """Calculate and return comfortable difficulty levels (in-order) based on the supplied scores."""
     comfortable_difficulty = None
     max_difficulty_low_score = None
     min_difficulty_high_score = None
@@ -170,6 +193,19 @@ def calculate_comfortable_difficulties(difficulty_scores):
             *reversed(difficulty_levels[:comfortable_difficulty_index]),
             *difficulty_levels[comfortable_difficulty_index + 1:],
         ]
+
+
+def get_uncomfortable_difficulties(comfortable_difficulties):
+    """Calculate and return uncomfortable difficulty levels (in-order), using the supplied comfortable difficulties."""
+    comfortable_difficulty = comfortable_difficulties[0]
+    if comfortable_difficulty + 1 <= max(difficulty_levels):
+        comfortable_difficulty += 1
+    comfortable_difficulty_index = difficulty_levels.index(comfortable_difficulty)
+    return [
+        comfortable_difficulty,
+        *difficulty_levels[comfortable_difficulty_index + 1:],
+        *reversed(difficulty_levels[:comfortable_difficulty_index]),
+    ]
 
 
 def get_uncomfortable_concepts_or_contexts(scores, category_nums):
