fce920c6077b7c3da851d2715e779d7552ca8c0d
Jack Morgan <jackmorgannz@gmail.com>
2022-09-27 10:23:30 +1300

    Simplify test password generation logic

--- codewof/tests/users/factories.py
+++ codewof/tests/users/factories.py
@@ -13,22 +13,8 @@ class UserFactory(DjangoModelFactory):
     email = factory.Faker("email")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
+    password = factory.Faker("password")
     user_type = factory.Iterator(UserType.objects.all())
-
-    @factory.post_generation
-    def password(self, create: bool, extracted: Sequence[Any], **kwargs):
-        """Create password for user."""
-        password = factory.Faker(
-            "password",
-            length=42,
-            special_chars=True,
-            digits=True,
-            upper_case=True,
-            lower_case=True,
-        ).generate(
-            extra_kwargs={}
-        )
-        self.set_password(password)
 
     class Meta:
         """Metadata for UserFactory class."""
