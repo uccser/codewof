8412518e2549aa5b68afebf68194ff7ce119efd9
lydia Jackson <lydiajnz@gmail.com>
2026-05-22 12:53:46 +1200

    Add debugging a function question

---
+++ codewof/programming/content/en/taylor-swifts-favourite-number/initial.py
@@ -0,0 +1,9 @@
+def taylor_swifts_favourite_number(number_guess):
+    taylors_favourite_number = 13
+    if number_guess == taylors_favourite_number:
+        print("This is Taylor Swifts favourite number!")
+    else if number_guess < taylors_favourite_number:
+        print("This number is {taylors_favourite_number - number_guess} less than Taylor Swifts favourite number.")
+    else:
+        print(f"This number is {number_guess - taylors_favourite_number} more than Taylor Swifts favourite number.")
+
---
+++ codewof/programming/content/en/taylor-swifts-favourite-number/question.md
@@ -0,0 +1,5 @@
+# Taylor Swift's Favourite Number
+
+Here is a function takes a integer input and tells you how close it is to Taylor Swifts favourite number. 
+Find the bugs and fix them so that the function works correctly and the tests pass.
+(HINT: there are 2 bugs to find.)
